fb5cb9a7421d0ba69cad1188fb8d47dfab43a88f
planetoftheweb <ray@planetoftheweb.com>
2022-03-31 20:43:43 +0000

    Deploying to root from @ planetoftheweb/planetoftheweb.github.io@e8fc3237c2bc990e01165f6d549bfbeb7449f3ba 🚀

--- js/data.js
+++ js/data.js
@@ -9,7 +9,7 @@
   "title": "Sass Essential Training",
   "date" : "Mar 03, 2022",
   "summary" : "Learn the fundamentals of Syntactically Awesome Stylesheets (Sass), a modern web development language that helps you write CSS better, faster, and with more advanced features.",
-  "thumbnail": "/images/courses/sass-essential-training_tn.png",
+  "thumbnail": "/images/courses/sass-essential-training_tn.jpg",
   "url": "https://linkedin-learning.pxf.io/c/1252977/449670/8005?u=https%3A%2F%2Fwww.linkedin.com%2Flearning%2Fsass-essential-training",
   "tags" : "external,video tutorial,css,sass,html,web designer,web developer"
 },{
