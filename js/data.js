1046e09b08cef349694bf9abe8fe5de3ecf72b31
planetoftheweb <ray@planetoftheweb.com>
2022-04-07 06:48:53 +0000

    Deploying to root from @ planetoftheweb/planetoftheweb.github.io@12a5d0109a781f9909e63110a10cf08d7f0b4e1a 🚀

--- js/data.js
+++ js/data.js
@@ -6,6 +6,13 @@
   "url": "/toolbox/2022-04-06-whats-next-js-ep23/",
   "tags" : ""
 },{
+  "title": "Styles Master",
+  "date" : "Apr 06, 2022",
+  "summary" : "This is a unique new framework that looks like something trying to be a lot like Tailwind CSS. It provides a way for you to create a website without using a stylesheet by exposing a series of commands through class attributes. You can adjust media queries and have 172 styles out of the box. You can add lots of effects. Looks real interesting.",
+  "thumbnail": "/images/toolbox/2022-04-07_01-54-11.png",
+  "url": "https://styles.master.co/",
+  "tags" : "external"
+},{
   "title": "Cirrus",
   "date" : "Apr 05, 2022",
   "summary" : "This is a real interesting Development because it&#39;s basically Tailwind CSS, but for Sass. The advantage is that all of the configuration is done in sass instead of through JS config.",
